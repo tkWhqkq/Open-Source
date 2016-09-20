5d460974bb6b90a2aec38085fb61fc4e7b8c0e8e
tkWhqkq <qnftp26@naver.com>
2016-09-20 20:31:50 +0900

    Add files via upload

---
+++ ex08-8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("프로그램 개발 과정:");
+	printf("\n");
+	printf("\n");
+	printf("1. 솔루션과 프로젝트 작성\n");
+	printf("2. 소스 작성\n");
+	printf("3. 빌드\n");
+	printf("4. 실행\n");
+	
+
+	return 0;
+
+}k
